395fa37a4c034d670a0408e5a545324702d424d2
Nurisnaini <133099686+anugrahwd@users.noreply.github.com>
2023-10-08 14:50:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(router);
-app.use("/", express.static(path.join(__dirname, "FE-Jayapura-28")));
 
 app.listen(port, function() {
     console.log("server running on", port);
